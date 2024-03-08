ccc70463ecb9627f4febbd3ed42313a18818a8b8
Baylee Schmeisser <github.mx@baylee.dev>
2024-03-07 19:19:06 -0800

    📛 Update city name.

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ const latitude = parseFloat(args[0]) || 45.523
 const longitude = parseFloat(args[1]) || -122.676
 const tz = args[2] || 'pt'
 
-const pdx = new CityWeather({
+const city = new CityWeather({
   latitude,
   longitude,
   tz,
 })
 
-console.log('Overcast Score', await pdx.overcastScore())
-console.log('Avg Temp', await pdx.avgTemp())
-console.log('Sunshine Score', await pdx.sunshineScore())
+console.log('Overcast Score', await city.overcastScore())
+console.log('Avg Temp', await city.avgTemp())
+console.log('Sunshine Score', await city.sunshineScore())
